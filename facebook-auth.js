06dcaab45983449509fab55e9baf669a13ec4e0c
mirceaalexandru <mircea.alexandru@gmail.com>
2015-07-09 17:11:55 +0300

    Accept parameters that are sent to passport strategy. These parameters can be used to request special user data permissions. Added separate seneca action for preparing user data. Added description to Readme.md file.

--- facebook-auth.js
+++ facebook-auth.js
@@ -1,29 +1,53 @@
 
 var FacebookStrategy = require('passport-facebook').Strategy
+var _ = require('lodash')
 
 module.exports = function (options) {
 
   var seneca = this
 
-  var authPlugin = new FacebookStrategy({
-      clientID:       options.appId,
-      clientSecret:   options.appSecret,
-      callbackURL:    options.urlhost + "/auth/facebook/callback"
-    },
+  var params = {
+    clientID:       options.appId,
+    clientSecret:   options.appSecret,
+    callbackURL:    options.urlhost + "/auth/facebook/callback"
+  }
+  params = _.extend(params, options.serviceParams || {})
+  var authPlugin = new FacebookStrategy(params,
     function (accessToken, refreshToken, profile, done) {
-      var data = {
-        identifier: '' + profile.id,
-        nick: profile.displayName,
-        username: profile.username,
-        credentials: {
-          access: accessToken,
-          refresh: refreshToken},
-        userdata: profile,
-        when: new Date().toISOString()
-      };
-      done(null, data)
+      seneca.act({role: 'facebook', cmd: 'prepareLoginData', accessToken: accessToken, refreshToken: refreshToken, profile: profile}, done)
     }
   )
+
+  var prepareLoginData = function(args, cb){
+    var accessToken = args.accessToken
+    var refreshToken = args.refreshToken
+    var profile = args.profile
+
+    var data = {
+      identifier: '' + profile.id,
+      nick: profile.displayName,
+      username: profile.username,
+      credentials: {
+        access: accessToken,
+        refresh: refreshToken},
+      userdata: profile,
+      when: new Date().toISOString()
+    };
+
+    data = _.extend({}, data, profile)
+    if (data.emails && data.emails.length > 0){
+      data.email = data.emails[0].value
+    }
+    if (data.name && _.isObject(data.name)){
+      data.firstName = data.name.givenName
+      data.lastName = data.name.familyName
+      delete data.name
+    }
+    data.name = data.name || (data.firstName + ' ' + data.lastName),
+      cb(null, data)
+  }
+
+  seneca.add({role: 'facebook', cmd: 'prepareLoginData'}, prepareLoginData)
 
   seneca.act({role: 'auth', cmd: 'register_service', service: 'facebook', plugin: authPlugin, conf: options})
 
